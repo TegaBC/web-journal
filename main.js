1b4cb5177c4dffacaac2383029fc573ca48958c1
TegaBC <jjbencarew10@gmail.com>
2023-02-05 19:32:27 +0000

    added basic functionality

--- main.js
+++ main.js
@@ -1,32 +1,88 @@
-
+const CREATE_BUTTON = document.querySelector("#create")
 const SAVE_BUTTON = document.querySelector("#save")
-const ENTRY_TEXT = document.querySelector("#entry")
-const SELECTOR = document.querySelector(".selector")
+const BACK_BUTTON = document.querySelector("#back")
+const ENTRY_TEXT = document.querySelector("#entry") // text area
+
+const EDITOR = document.querySelector(".editor")
 const DATE = document.querySelector("#date-picker")
+
+const SELECTOR = document.querySelector(".selector")
+const DISPLAY = document.querySelector(".display")
+const PREVIEW = document.querySelector(".preview")
+const MENU = document.querySelector(".menu")
+
+const VIEWING_LABEL = document.querySelector("#viewing")
 
 let storage = {} 
 
+// inital storage load
+
+if (localStorage.getItem("entries") != null) {
+    let storedItems = JSON.parse(localStorage.getItem("entries"))
+
+    for (const [date, text] of Object.entries(storedItems)) {
+        storage[date] = text
+        addButton(date)
+    }
+}
+
+function addButton(date) {
+    // create a entry button
+    let entryButton = document.createElement("button")
+    entryButton.classList.add("entry-selector")
+    entryButton.textContent = date
+
+    // give button functionality
+    entryButton.addEventListener("click", () => {
+        VIEWING_LABEL.innerHTML = "Currently viewing: " + date
+
+        MENU.style.display = "none"
+        EDITOR.style.display = "none"
+        DISPLAY.style.display = "flex"
+
+        PREVIEW.innerHTML = storage[date]
+    })
+
+     // add button to the selector reel
+     SELECTOR.appendChild(entryButton)
+}
+
 function saveEntry() {
-    // check for any ones
-    // check if a date is chosen
-    if (!DATE.value) {
+    // save current date
+    let currentDate = DATE.value
+
+    if (!currentDate) {
         alert("Pick a date for this entry!")
         return
     }
-    else if (storage[DATE.value]) {
+    else if (storage[currentDate]) {
         alert("An entry for this date has already been saved")
         return
     }
 
-    // bind the current date to the text saved.
-    storage[DATE.value] = ENTRY_TEXT.value
-    // create a entry button
-    let entryButton = document.createElement("button")
-    entryButton.classList.add("entry-selector")
-    entryButton.textContent = DATE.value
-    // add button to the selector reel
-    SELECTOR.appendChild(entryButton)
+    // bind the current date to the text saved & save
+    storage[currentDate] = ENTRY_TEXT.value
+    localStorage.setItem("entries", JSON.stringify(storage))
+   
+    // generate button
+    addButton(currentDate)
+    
+    // hide the editor
+    MENU.style.display = "flex"
+    EDITOR.style.display = "none"
 }
-
-// watch for any events
+ 
 SAVE_BUTTON.addEventListener("click", saveEntry)
+
+CREATE_BUTTON.addEventListener("click", () => {
+    ENTRY_TEXT.value = ""
+
+    DISPLAY.style.display = "none"
+    MENU.style.display = "none"
+    EDITOR.style.display = "flex"
+} )
+
+BACK_BUTTON.addEventListener("click", () => {
+    DISPLAY.style.display = "none"
+    MENU.style.display = "flex"
+})
