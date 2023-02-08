eeb5b9fa04760a2a6520b4cdd0347631296b78c6
TegaBC <jjbencarew10@gmail.com>
2023-02-08 22:12:43 +0000

    added editing functionality

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ const CREATE_BUTTON = document.querySelector("#create")
 const SAVE_BUTTON = document.querySelector("#save")
 const BACK_BUTTON = document.querySelector("#back")
 const DELETE_BUTTON = document.querySelector("#delete")
+const EDIT_BUTTON = document.querySelector("#edit")
+const SAVE_EDIT_BUTTON = document.querySelector("#save-edit")
+
+const EDITOR_BUTTONS = [SAVE_EDIT_BUTTON, BACK_BUTTON, DELETE_BUTTON, EDIT_BUTTON]
+
 const ENTRY_TEXT = document.querySelector("#entry") // text area
+const EDIT_ENTRY_AREA = document.querySelector("#edit-entry")
 
 const EDITOR = document.querySelector(".editor")
 const DATE = document.querySelector("#date-picker")
@@ -42,12 +48,14 @@ function addButton(date) {
 
     // give button functionality
     entryButton.addEventListener("click", () => {
-        // Set label
-        VIEWING_LABEL.innerHTML = "Currently viewing: " + date
+        // Set label 
+        VIEWING_LABEL.innerHTML = date
 
         // Set what displays
         MENU.style.display = "none"
         EDITOR.style.display = "none"
+        EDIT_ENTRY_AREA.style.display = "none"
+        PREVIEW.style.display = "flex"
         DISPLAY.style.display = "flex"
 
         // Push saved text to screen
@@ -55,11 +63,37 @@ function addButton(date) {
 
         // Update what we are viewing
         currentViewing = date
+
+        // make sure we are seeing the correct buttons
+        EDITOR_BUTTONS.forEach((button) => {
+            button.style.display = "inline-block"
+        })
+
+        SAVE_EDIT_BUTTON.style.display = "none"
     })
 
      // add button to the selector reel
      reelButtons[date] = entryButton
      SELECTOR.appendChild(entryButton)
+}
+
+function editButton() {
+    // get current text
+    let entryText = storage[currentViewing]
+
+    // display edit box
+    PREVIEW.style.display = "none"
+    EDIT_ENTRY_AREA.style.display = "flex"
+    
+    EDIT_ENTRY_AREA.innerHTML = entryText
+ 
+    // hide all buttons, except save
+    EDITOR_BUTTONS.forEach((button) => {
+        button.style.display = "none"
+    })
+
+    SAVE_EDIT_BUTTON.style.display = "inline-block"
+    // back to menu like normal
 }
 
 function saveEntry() {
@@ -87,7 +121,32 @@ function saveEntry() {
     EDITOR.style.display = "none"
 }
  
+function saveEdit() {
+    let newText = EDIT_ENTRY_AREA.value
+
+    // save text
+    storage[currentViewing] = newText
+    saveStorage()
+
+    // update preview text
+    PREVIEW.innerHTML = newText
+
+    // show the entry like normal, make editor go away
+    EDIT_ENTRY_AREA.style.display = "none"
+    PREVIEW.style.display = "flex"
+    DISPLAY.style.display = "flex"
+
+    // show correct buttons
+    EDITOR_BUTTONS.forEach((button) => {
+        button.style.display = "inline-block"
+    })
+
+    SAVE_EDIT_BUTTON.style.display = "none"
+}
+
+SAVE_EDIT_BUTTON.addEventListener("click", saveEdit)
 SAVE_BUTTON.addEventListener("click", saveEntry)
+EDIT_BUTTON.addEventListener("click", editButton)
 
 DELETE_BUTTON.addEventListener("click", () => {
     // remove from storage and save
